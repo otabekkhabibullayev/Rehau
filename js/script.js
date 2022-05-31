52a5fcf499e32b8b38c93c819846e6139a8d85f1
Otabek <otashkin25@gmail.com>
2022-06-01 01:14:10 +0500

    some changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var L;
-window.onload = function () {
+window.onload = function() {
     L.mapquest.key = '5TB5t0zArYbHozfdlkxM2Az3bONaw56G';
     var map = L.mapquest.map('map', {
         center: [53.480759, -2.242631],
@@ -7,25 +7,25 @@ window.onload = function () {
         zoom: 12
     });
     L.marker([53.480759, -2.242631], {
-        icon: L.mapquest.icons.marker({
-            primaryColor: '#22407F',
-            secondaryColor: '#3B5998',
-            shadow: true,
-            size: 'md',
-            symbol: ''
+            icon: L.mapquest.icons.marker({
+                primaryColor: '#22407F',
+                secondaryColor: '#3B5998',
+                shadow: true,
+                size: 'md',
+                symbol: ''
+            })
         })
-    })
         .bindPopup('This is Manchester!')
         .addTo(map);
 }
 
 
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+    document.getElementById("myNav").style.height = "100%";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav").style.height = "0%";
 }
 
 const call = document.querySelector('#call');
@@ -33,12 +33,12 @@ const modal = document.querySelector('.modal_wrapper');
 
 const x = document.querySelector('#x');
 
-call.addEventListener('click', ()=>{
-  modal.style.display = 'block'
+call.addEventListener('click', () => {
+    modal.style.display = 'block'
 })
 
-x.addEventListener('click', ()=>{
-  modal.style.display = 'none'
+x.addEventListener('click', () => {
+    modal.style.display = 'none'
 })
 
 const input = document.querySelector('#inp');
@@ -46,13 +46,14 @@ const label = document.querySelector('#lbl');
 input.onfocus = labelCss
 input.onblur = defaault
 
-function labelCss(){
-  label.style.fontSize = '14px'
-  label.style.top = '44%'
-  // console.log('hello');
+function labelCss() {
+    label.style.fontSize = '14px'
+    label.style.top = '44%'
+        // console.log('hello');
 }
-function defaault(){
-  label.style.fontSize = '18px'
-  label.style.top = '48%'
-  // console.log('hello');
+
+function defaault() {
+    label.style.fontSize = '18px'
+    label.style.top = '48%'
+        // console.log('hello');
 }
